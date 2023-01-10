5b7e494e0c74851012d7e410ed841dbc16bc7236
Marc Faussurier <faussurier.marc@icloud.com>
2023-01-10 16:03:24 +0100

    btn order

--- nav.js
+++ nav.js
@@ -23,13 +23,13 @@ let nav_history;
 function init_nav()
 {
     nav_direction = "initial page";
-    nav_position = 1;
-    nav_history = [{
+    nav_position = 0;
+    nav_history = [/*{
         url: document.location.href,
         state: {
             [nav_state_time_key]: new Date().getTime()
         }
-    }
+    }*/
     ];
 }
 
@@ -55,11 +55,14 @@ window.addEventListener('nav::tick', function(e) {
 
 
 function nav_dispatch(e = null) {
+    state = {};
+    if (nav_position)
+        nav_history[nav_position - 1].state;
     window.dispatchEvent(new CustomEvent('nav::tick', {detail: {
             nav_prev_position: nav_prev_position,
             nav_position: nav_position,
             nav_history: nav_history,
-            nav_state: nav_history[nav_position - 1].state,
+            nav_state: state,
             nav_direction: nav_direction,
             popstate_event: e
     }, cancelable: true}));
@@ -93,62 +96,6 @@ function is_nav_backward_possible()
         ((nav_position === 1) && (nav_history.length < window.history.length)));
 }
 
-window.history.pushState = hook(window.history.pushState, function (cb, ...args){
-    console.log('nav::pushState(...)');
-    if (args.length < 2 || typeof args[0] !== 'object'
-        || args[2] === window.location.pathname
-    )
-        return cb(args);
-    args[0][nav_state_time_key] = new Date().getTime()
-    args[0][nav_url_key] = args[2];
-    if (!nav_dispatch_before("pushState", args))
-        return false;
-    const value = cb(args);
-    nav_history.push({url: document.location.href, state: args[0]});
-    nav_position = nav_history.length;
-    nav_direction = "forward";
-    nav_dispatch();
-    return (value);
-});
-
-window.history.replaceState = hook(window.history.replaceState, function (cb, ...args) {
-    console.log('nav::replaceState(...)');
-    //throw new Error("ERRRR");
-    if (!nav_dispatch_before("replaceState", args) && !args[0].force) {
-        console.log("FAILED SAVED == 0");
-        return false;
-    }
-    if (args.length < 2 || typeof args[0] !== 'object'
-        || args[2] === window.location.pathname)
-        return cb(args);
-    args[0][nav_state_time_key] = new Date().getTime()
-    args[0][nav_url_key] = args[2];
-
-    nav_history[nav_position - 1] = {url: args[2], state: args[0]};
-    const value = cb(args);
-    nav_direction = "redirection";
-    nav_dispatch();
-    return (value);
-});
-
-window.history.go = hook(window.history.go, function (cb, ...args) {
-    console.log('nav::go(...)', args);
-    if (!nav_dispatch_before("go", args))
-        return false;
-    nav_prev_position = nav_position;
-
-    const value = cb(args);
-    if (args.length < 1 || isNaN(args[0]))
-        return value;
-    args[0] = Math.round(args[0]);
-    if (args[0] === 0)
-        return value;
-    if ((nav_position + args[0] >= 1)
-         ||
-        (nav_position + args[0] <= nav_history.length))
-            nav_position += args[0];
-    return (value);
-});
 
 window.addEventListener("popstate", function(e) {
     console.log('nav::onPopState 42');
@@ -195,3 +142,70 @@ window.addEventListener("popstate", function(e) {
         nav_dispatch(e);
     }
 });
+
+function init_hooks()
+{
+    window.history.pushState = hook(window.history.pushState, function (cb, ...args){
+        console.log('nav::pushState(...)');
+        if (args.length < 2 || typeof args[0] !== 'object'
+            || args[2] === window.location.pathname
+        )
+            return cb(args);
+        args[0][nav_state_time_key] = new Date().getTime()
+        args[0][nav_url_key] = args[2];
+        if (!nav_dispatch_before("pushState", args))
+            return false;
+        const value = cb(args);
+        nav_history.push({url: document.location.href, state: args[0]});
+        nav_position = nav_history.length;
+        nav_direction = "forward";
+        nav_dispatch();
+        return (value);
+    });
+
+    window.history.replaceState = hook(window.history.replaceState, function (cb, ...args) {
+        console.log('nav::replaceState(...)');
+        //throw new Error("ERRRR");
+        if (!nav_dispatch_before("replaceState", args) && !args[0].force) {
+            console.log("FAILED SAVED == 0");
+            return false;
+        }
+        if (args.length < 2 || typeof args[0] !== 'object'
+            || args[2] === window.location.pathname)
+            return cb(args);
+        args[0][nav_state_time_key] = new Date().getTime()
+        args[0][nav_url_key] = args[2];
+
+        nav_history[nav_position - 1] = {url: args[2], state: args[0]};
+        const value = cb(args);
+        nav_direction = "redirection";
+        nav_dispatch();
+        return (value);
+    });
+
+    window.history.go = hook(window.history.go, function (cb, ...args) {
+        console.log('nav::go(...)', args);
+        if (!nav_dispatch_before("go", args))
+            return false;
+        nav_prev_position = nav_position;
+
+        const value = cb(args);
+        if (args.length < 1 || isNaN(args[0]))
+            return value;
+        args[0] = Math.round(args[0]);
+        if (args[0] === 0)
+            return value;
+        console.log("cur:", nav_position);
+        if ((nav_position + args[0] >= 1)
+            ||
+            (nav_position + args[0] <= nav_history.length))
+            nav_position += args[0];
+        nav_direction = args[0] < 0 ? "backward" : "forward";
+        nav_dispatch();
+        console.log("new:", nav_position);
+        return (value);
+    });
+
+}
+
+init_hooks();
